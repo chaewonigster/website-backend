214bb354b48c0a248dfced30cec484001d62be3b
nathaniel <jarmenathaniel@gmail.com>
2025-10-28 01:39:59 +0800

    Fix root route to show backend status

--- server.js
+++ server.js
@@ -8,11 +8,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.get("/", (req, res) => {
-  res.send("🎉 Backend is live!");
-});
-
-app.use("/app", express.static(path.join(__dirname, "Public")));
+app.use(express.static(path.join(__dirname, "Public")));
+app.use(express.static(path.join(__dirname)));
 
 app.get("/admin.html", (req, res) => {
   res.sendFile(path.join(__dirname, "Public", "admin.html"));
